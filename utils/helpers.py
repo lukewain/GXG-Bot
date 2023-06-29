98daae5ff371a8b4aaca2820480b20f01e861f58
lukewain <lukewainhouse@outlook.com>
2023-06-29 09:48:59 +0100

    Added helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 import discord
 
+from datetime import datetime
 from dataclasses import dataclass
 import asyncpg
 import json
 
-from src.bot import GXGInteraction
+from typing import Optional
+
+# from src.bot import GXGInteraction
 
 __all__ = ("ModerationLog", "Configuration")
 
@@ -23,12 +26,12 @@ class Configuration:
 
         results = data.values()
 
-        return cls(**results)
+        return cls(**data)
 
     async def validate_warn_threshold(
-        self, interaction: GXGInteraction, user: discord.Member
+        self, interaction: discord.Interaction, user: discord.Member
     ) -> bool:
-        res: int = await interaction.pool.fetchval(
+        res: int = await interaction.pool.fetchval(  # type: ignore
             "SELECT infraction from warnings WHERE id=$1", user.id
         )
 
@@ -42,7 +45,7 @@ class Configuration:
 class ModerationLog:
     entry_id: int
     moderator_id: int
-    timestamp: int
+    unixtimestamp: int
     action: str
     reason: str
     moderatee_id: int
@@ -63,7 +66,37 @@ class ModerationLog:
         id: `int`
             The moderator id to find actions of
         """
-        query = "SELECT * FROM moderationlog WHERE moderator_id=$1 ORDER BY timestamp DESC LIMIT 10"
+        query = "SELECT * FROM moderationlog WHERE moderator_id=$1 ORDER BY unixtimestamp DESC LIMIT 10"
+
+        res: list[asyncpg.Record] = await pool.fetch(query, id)
+
+        if len(res) < 1:
+            return None
+
+        return [cls(**r) for r in res]
+
+    @classmethod
+    async def get_moderatee_logs(
+        cls, pool: asyncpg.Pool, id: int, /
+    ) -> list[ModerationLog] | None:
+        """
+        |coro|
+
+        Finds all the moderation logs linked to a specific user
+
+        Paramters
+        ---------
+        pool: `asyncpg.Pool`
+            The client pool to use
+        id: `int`
+            The moderatee you want to view
+
+        Returns
+        -------
+        `ModerationLog` or `None`
+        """
+
+        query = "SELECT * FROM moderationlog WHERE moderatee_id=$1 ORDER BY unixtimestamp DESC"
 
         res: list[asyncpg.Record] = await pool.fetch(query, id)
 
@@ -114,7 +147,7 @@ class ModerationLog:
         )
         fields = {
             "Moderator": f"<@{self.moderator_id}>",
-            "Perpertrator": f"<@{self.moderatee_id}>",
+            "User": f"<@{self.moderatee_id}>",
             "Action": self.action,
             "Reason": self.reason,
         }
@@ -122,3 +155,109 @@ class ModerationLog:
             embed.add_field(name=k, value=v, inline=False)
 
         return embed
+
+
+@dataclass
+class Muted:
+    mute_id: int
+    id: int
+    reason: Optional[str]
+    duration: int
+    expires: int
+    expired: bool
+
+    @classmethod
+    async def fetch_next(cls, pool: asyncpg.Pool) -> Muted | None:
+        now = round(datetime.now().timestamp())
+
+        res: asyncpg.Record = await pool.fetchrow(
+            "SELECT * FROM muted WHERE expires > $1 AND expired = $2 ORDER BY expires DESC LIMIT 1",
+            now,
+            False,
+        )
+
+        return cls(**res)
+
+    @classmethod
+    async def add_new(
+        cls, pool: asyncpg.Pool, id: int, reason: Optional[str], duration: int
+    ) -> Muted:
+        query = (
+            "INSERT INTO muted (id, reason, duration, expires) VALUES ($1, $2, $3, $4)"
+        )
+
+        now = round(datetime.now().timestamp())
+
+        expiration = now + duration
+
+        res: asyncpg.Record = await pool.fetchrow(
+            query, id, reason, duration, expiration
+        )
+
+        return cls(**res)
+
+    @classmethod
+    async def check(cls, pool: asyncpg.Pool, id: int) -> Muted | None:
+        query = "SELECT * FROM muted WHERE id=$1 AND expired=$2"
+
+        res: asyncpg.Record | None = await pool.fetchrow(query, id, False)
+
+        if not res:
+            return None
+
+        return cls(**res)
+
+    @classmethod
+    async def premature(cls, pool: asyncpg.Pool, id: int) -> Muted | None:
+        query = "SELECT * FROM muted WHERE id=$1 and expired=$2"
+
+        res: asyncpg.Record | None = await pool.fetchrow(query, id, False)
+
+        if res:
+            now = round(datetime.now().timestamp())
+            query = (
+                "UPDATE muted SET expired=$1, expires=$2 WHERE mute_id=$3 RETURNING *"
+            )
+            res2 = await pool.fetchrow(query, True, now, res["mute_id"])
+
+            return cls(**res2)
+
+        else:
+            return None
+
+
+@dataclass
+class Warning:
+    id: int
+    infractions: int
+    infraction_reasons: list[str]
+
+    @classmethod
+    async def add(cls, pool: asyncpg.Pool, id: int, reason: str) -> Warning:
+        res = await pool.fetchrow("SELECT * FROM warnings WHERE id=$1", id)
+
+        if res:
+            data = await pool.fetchrow(
+                "UPDATE warnings SET infractions = infractions + 1, infraction_reasons = array_append(infraction_reasons, $1) WHERE id=$2 RETURNING *",
+                reason,
+                id,
+            )
+        else:
+            data = await pool.fetchrow(
+                "INSERT INTO warnings (id, infractions, infraction_reasons) VALUES ($1, $2, $3) RETURNING *",
+                id,
+                1,
+                [reason],
+            )
+
+        return cls(**data)
+
+    @classmethod
+    async def fetch(cls, pool: asyncpg.Pool, id: int) -> Warning | None:
+        res = await pool.fetchrow("SELECT * FROM warnings WHERE id=$1", id)
+
+        if not res:
+            return None
+
+        else:
+            return cls(**res)
